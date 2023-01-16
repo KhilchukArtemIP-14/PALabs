5860c25b9176ce59ae22a0d1343338db07e8589f
KhilchukArtemIP-14 <89968135+KhilchukArtemIP-14@users.noreply.github.com>
2023-01-16 18:54:35 +0200

    Initial commit

---
+++ PA_Lab4/PALab4/PALab4/Ants/IAnt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PALab3.Ants
+{
+    interface IAnt
+    {
+        public int CalculateGreedyPath(int length);
+        public IEnumerable<Tuple<City,City>> SeekPath(int length);
+        public void Refresh();
+        public int GetLMin();
+        public int GetPathLength();
+        public Tuple<City, City> ReturnToStart();
+    }
+}
